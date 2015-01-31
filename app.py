881ce1145c986977120e147044123e4e8248736b
Keyan Pishdadian <kpishdadian@gmail.com>
2015-01-31 16:04:32 -0500

    Fix Procfile

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from itertools import count
 import sqlite3
+from contextlib import closing
 from flask import Flask, render_template, request, redirect, session, g, \
      abort, flash, url_for
 
@@ -31,6 +32,13 @@ def shorten(redirect_id):
     return redirect(redirection)
 
 
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resorce('schemea.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
@@ -38,4 +46,4 @@ def connect_db():
 if __name__ == "__main__":
     url_list = {}
     id_gen = count(1, 1)
-    app.run(debug=True)
+    app.run()
